418e9bc25d0263dc1b8884f97061e047c954153f
Surajk37 <surajkhot9993@gmail.com>
2022-12-14 20:02:29 +0530

    Calculated Factorial Of Number

---
+++ BasicProgram/FactorialNo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram
+{
+    public class FactorialNo
+    {
+        public static void Fact()
+        {
+            int no, fact;
+            Console.WriteLine("Enter A Number");
+            no = Convert.ToInt32(Console.ReadLine());
+
+            fact = no;
+
+            for (int i = no - 1; i >= 1; i--)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("Factorial Number is : {0}", fact);
+            Console.ReadLine();
+        }
+    }
+}
+
+ 
