47cc3a8e741993106bac6e073f3c241beef80af5
Surajk37 <surajkhot9993@gmail.com>
2022-12-14 21:10:58 +0530

    Febonacci Series Program Uploaded

---
+++ BasicProgram/FibonacciSeries.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram
+{
+    internal class FibonacciSeries
+    {
+        public void Fibonacci(int n)
+        {
+            int val1 = 0, val2 = 1, val3;
+            Console.Write(+ val1+ " "+ val2+ " ");
+            for(int i = 2; i < n; i++)
+            {
+                val3 = val1 + val2;
+                Console.Write(val3+" ");
+                val1 = val2;
+                val2 = val3;
+            }
+        }
+    }
+}
--- BasicProgram/Program.cs
+++ BasicProgram/Program.cs
@@ -8,13 +8,19 @@
 
             //PrimeNumber primenumber = new PrimeNumber();
             //primenumber.ToCheckPrime();
-            Console.WriteLine("\tPlease enter number");
-            int sk = Convert.ToInt32(Console.ReadLine());
-            Reverse.ReverseNumber(sk);
-            Console.WriteLine("-------------------------------------");
+            //Console.WriteLine("\tPlease enter number");
+            //int sk = Convert.ToInt32(Console.ReadLine());
+            //Reverse.ReverseNumber(sk);
+            //Console.WriteLine("-------------------------------------");
 
-            Console.WriteLine("\n\tPlease enter any word to make reverse");
-            Reverse.ReverseWord(Console.ReadLine());
+            //Console.WriteLine("\n\tPlease enter any word to make reverse");
+            //Reverse.ReverseWord(Console.ReadLine());
+            Console.WriteLine("\t\tFebonacci Series Problem\n");
+            Console.WriteLine("Enter a Range ");
+            int num = int.Parse(Console.ReadLine());
+            FibonacciSeries fibonacciSeries = new FibonacciSeries();
+            fibonacciSeries.Fibonacci(num);
+
 
         }
     }
