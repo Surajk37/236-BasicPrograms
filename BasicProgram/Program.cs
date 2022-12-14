69493752b3757eeb96e8740a9472f1f01a37d6da
Surajk37 <surajkhot9993@gmail.com>
2022-12-14 20:38:00 +0530

    Reversed the Number and String

--- BasicProgram/Program.cs
+++ BasicProgram/Program.cs
@@ -6,9 +6,16 @@
         {
             //FactorialNo.Fact();
 
-            PrimeNumber primenumber = new PrimeNumber();
-            primenumber.ToCheckPrime();
-    
+            //PrimeNumber primenumber = new PrimeNumber();
+            //primenumber.ToCheckPrime();
+            Console.WriteLine("\tPlease enter number");
+            int sk = Convert.ToInt32(Console.ReadLine());
+            Reverse.ReverseNumber(sk);
+            Console.WriteLine("-------------------------------------");
+
+            Console.WriteLine("\n\tPlease enter any word to make reverse");
+            Reverse.ReverseWord(Console.ReadLine());
+
         }
     }
 }
---
+++ BasicProgram/Reverse.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram
+{
+    public class Reverse
+    {
+        public static void ReverseNumber(int number)//5367
+        {
+            int remainder = 0, reverse = 0;
+            while (number != 0)
+            {
+                remainder = number % 10; //7  
+                reverse = (reverse * 10) + remainder; // 7
+                number = number / 10; //536
+                Console.WriteLine("Remainder:{0} number:{1} reverse:{2}", remainder, number, reverse);
+            }
+
+            Console.WriteLine("Reverse number is : " + reverse);
+        }
+       
+        public static void ReverseWord(string word)
+        {
+            string reverse = "";
+            char[] setOfCharacters = word.ToCharArray();
+            Console.WriteLine("\nDisplaying each character");
+            for (int i = 0; i < setOfCharacters.Length; i++)
+            {
+                Console.Write(setOfCharacters[i] + " ");
+            }
+            for (int i = 0; i < setOfCharacters.Length; i++)
+            {
+                reverse = setOfCharacters[i] + reverse;
+            }
+            Console.WriteLine("\n" + reverse);
+
+        }
+    }
+}
+
